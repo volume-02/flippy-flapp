f1b44db3c36e3adabcd0ff034a7916f44c4da4a3
volume02 <borsukboris@gmail.com>
2023-10-16 23:19:58 +0700

    bg image

--- Assets/Scripts/BackgroundScript.cs
+++ Assets/Scripts/BackgroundScript.cs
@@ -6,20 +6,26 @@ public class BackgroundScript : MonoBehaviour
 {
     private Vector3 startPos;
     private float repeatWidth;
+    public GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
-        repeatWidth = GetComponent<Renderer>().bounds.size.x / 2;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        gameManager = GameObject.FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * 5 * Time.deltaTime);
-        if (transform.position.x < startPos.x - repeatWidth)
-        {
-            transform.position = new Vector3(36, 0, 10);
-        }
+        //if (!gameManager.isGameOver)
+       // {
+            transform.Translate(Vector3.left * 15 * Time.deltaTime);
+            if (transform.position.x < startPos.x - repeatWidth)
+            {
+                transform.position = new Vector3(75, -5, -5);
+            }
+       // }
     }
 }
